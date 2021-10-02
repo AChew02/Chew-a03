ac0d3a35fa522ff4a25a4caf8a345992073c1448
Adrian Chew <achewinquiries@gmail.com>
2021-10-01 22:36:44 -0400

    ex24 pseudocode

---
+++ exercise24/src/main/java/Solution24.java
@@ -0,0 +1,29 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Adrian Chew
+ */
+
+public class Solution24 {
+
+    // print("Enter two strings and I'll tell you if they are anagrams:")
+    // print("Enter the first string: ")
+    // 'string1' = get input from user
+    // print("Enter the first string: ")
+    // 'string2' = get input from user
+    // 'stringLen1' = string1.length()
+    // 'stringLen2' = string2.length()
+    // if(stringLen1 != stringLen2)
+    //      'anagram' = false
+    // 'toSort[]' = string1.toCharArray()
+    // Arrays.sort('toSort')
+    // 'sortedString1' = String('toSort')
+    // 'toSort[]' = string2.toCharArray()
+    // Arrays.sort('toSort')
+    // 'sortedString2' = String('toSort')
+    // 'anagram' = sortedSting1.equals('sortedString2')
+    // if('anagram')
+    //      print("/"'string1'/" and /"'string2'/" are anagrams.")
+    // else
+    //      print("/"'string1'/" and /"'string2'/" are not anagrams.")
+
+}
