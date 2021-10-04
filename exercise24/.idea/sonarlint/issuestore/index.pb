2e210523a16a815ecb482e04e28753d9cdad1f86
Adrian Chew <achewinquiries@gmail.com>
2021-10-03 22:40:38 -0400

    ex28 pseudocode

---
+++ exercise24/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+M
+src/main/java/Solution24.java,e\7\e79d830cd7b618e70f47b42bf9f0a12787b152d5
+Q
+!src/test/java/Solution24Test.java,c\7\c731d7ca48acee7957ca9a57fe8f2a8fdabebad0
---
+++ exercise28/src/main/java/Solution28.java
@@ -0,0 +1,41 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Adrian Chew
+ */
+
+public class Solution28 {
+
+    public static void main(String[] args) {
+        Solution28 app = new Solution28();
+        //Declare sum, initalize to 0
+        int sum;
+
+        //Declare output string
+        String output;
+
+        //Loop 5 times to get input
+        for() {
+            //Get input from user, adding it to the sum
+            sum += getIntegerFromUser();
+        }
+
+        //Set output string based on sum
+        output = setOutput(sum);
+
+        //print output
+        printOutput(output);
+    }
+
+    private int getIntegerFromUser(String prompt) {
+        //Print prompt
+        //Return next int
+    }
+
+    public String setOutput(int sum) {
+        //return output message concatenated with sum
+    }
+
+    private void printOutput(String output, int sum) {
+        //print output string
+    }
+}
