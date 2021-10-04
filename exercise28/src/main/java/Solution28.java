6c6cfc950d5cae7fe710c47e89a6af37c354e70e
Adrian Chew <achewinquiries@gmail.com>
2021-10-03 22:47:55 -0400

    ex28 added code

--- exercise28/src/main/java/Solution28.java
+++ exercise28/src/main/java/Solution28.java
@@ -3,39 +3,48 @@
  *  Copyright 2021 Adrian Chew
  */
 
+import java.util.Scanner;
+
 public class Solution28 {
+
+    private static final Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
         Solution28 app = new Solution28();
-        //Declare sum, initalize to 0
-        int sum;
+        //Declare sum, initialize to 0
+        int sum = 0;
 
         //Declare output string
         String output;
 
         //Loop 5 times to get input
-        for() {
+        for(int i=0; i<5; i++) {
             //Get input from user, adding it to the sum
-            sum += getIntegerFromUser();
+            sum += app.getIntegerFromUser("Enter a number: ");
         }
 
         //Set output string based on sum
-        output = setOutput(sum);
+        output = app.setOutput(sum);
 
         //print output
-        printOutput(output);
+        app.printOutput(output);
     }
 
     private int getIntegerFromUser(String prompt) {
         //Print prompt
+        System.out.println(prompt);
+
         //Return next int
+        return in.nextInt();
     }
 
     public String setOutput(int sum) {
         //return output message concatenated with sum
+        return "The total is " + sum + ".";
     }
 
-    private void printOutput(String output, int sum) {
+    private void printOutput(String output) {
         //print output string
+        System.out.println(output);
     }
 }
